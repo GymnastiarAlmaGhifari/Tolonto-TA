c8bbe52e11aa8e02a0f1ced26b8a297ab1d132e1
Yoga Novaindra <yoga.november@gmail.com>
2022-11-16 17:41:04 +0700

    Header dan Register Api

--- api/registerApi.php
+++ api/registerApi.php
@@ -6,10 +6,12 @@ $userId = $_POST['user_id'];
 $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
+$hp = $_POST['hp'];
 $cookies = $_POST['cookies'];
 $create_at = $_POST['create_at'];
 $update_at = $_POST['update_at'];
 $saldo = $_POST['saldo'];
+$img = $_POST['img'];
 
 $create_at = date("Y-m-d H:i:s");
 $update_at = date("Y-m-d H:i:s");
@@ -20,10 +22,12 @@ if ($user->register_api(
         'email' => $email,
         'username' => $username,
         'password' => $password,
+        'hp' => $hp,
         'cookies' => $cookies,
         'create_at' => $create_at,
         'update_at' => $update_at,
         'saldo' => $saldo,
+        'img' => $img
     ]
 )) {
     $response = [
